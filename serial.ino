28524a8bccdf1921bd274b90e9b08c32c09db64b
Sven Bohm <bohms@msu.edu>
2014-11-20 11:26:56 -0500

    final program

--- serial.ino
+++ serial.ino
@@ -4,412 +4,285 @@
 #include <SD.h>	                                 // the SD Card library
 #include <SoftwareSerial.h>                      // soft serial ports
 #include <EEPROM.h>                              // built-in EEPROM routines
+#include <RTClib.h>
 
-
-int   Pingpower = 3;                             // power to Ping sensor
-int   SDpower = 9;                               // power to microSD card circuit
-int   SD_CS = 10;                                // ChipSelect pin for SDcard SPI
+const int   Pingpower = 3;                             // power to Ping sensor
+const int   SDpower = 7;                               // power to microSD card circuit
+const int   SD_CS = 10;                                // ChipSelect pin for SDcard SPI
 
 SoftwareSerial echoSerial =  SoftwareSerial(4, 5, true);  //RX,TX, inverted?
 Sleep sleep;
+RTC_DS1307 rtc;
+
 File myfile;                                     // initialize SDcard
 
-char c;
 double distance;
-unsigned long sleepTime = 500;                         //how long you want the arduino to sleep
-int   IDnum;                                     // Board/Site identifier
-char  name[] = "XXX-xxx.txt";                    // SDcard file
-  char  a[4];                                      // buffer array for itoa function 
+double lastDistance;
 
+const unsigned long sleepTime =  3600000;                         //how long you want the arduino to sleep
+int   IDnum = 13;                                               // Board/Site identifier
+char  name[] = "x-13.csv";                                  // SDcard file
 
-int   DS1337ctrl = B1101000;                     // DS1337 i2c identifier
-int   rtc_bcd[8];                                // time, date values
-int   rtc[8];
-int   secs;
-int   mins;
-int   hrs;
-int   dow;
-int   days;
-int   mnths;
-int   yrs;
+int i;
 
-int   menuinput;                                 // user input for menu 
-long  timeout;                                   // seconds before menu times out
-int   indata;                                    // user-input data
-int   input;
-int   addr;                                      // EEPROM address
+DateTime now;
+
+const int addr = 3;                                      // EEPROM address
 byte  EEPROMbyte;                                // EEPROM data to store/read
 
-
 void setup() {
-  int i;
-  delay(100);
   
+  // check for id number and set if zero
+  EEPROMbyte = EEPROM.read(addr);
+  
+  if (EEPROMbyte == 255) {
+    EEPROM.write(addr, IDnum);
+  }
+  IDnum = EEPROM.read(addr);
+  String filename = String(IDnum);
+  filename = filename + ".txt";
+  filename.toCharArray(name, sizeof(name));
+  
+  ADCSRA = 0;                                    // disable ADC
+ 
   pinMode(SDpower, OUTPUT);                      // power for SDcard
-  pinMode(SD_CS,OUTPUT);
-  
+  digitalWrite(SDpower, HIGH);		    // turn on SDcard LED at startup to verify operation
+  delay(100);
+
   Wire.begin();                                  // enable i2c bus
-
+  
   Serial.begin(9600);
-  echoSerial.begin(9600);
 
-  digitalWrite(SDpower,HIGH);		     // turn on SDcard LED at startup to verify operation
-  delay(1500);				     // wait
+  rtc.begin();  
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  }
+  now = rtc.now();
+
+  printDate();
+  Serial.println();
+
+  delay(500);				     // wait
 
   if (!SD.begin(SD_CS))                      // check SD card
   {
     Serial.println("initialization failed!");
 
-    for(i=1; i<7  ; i++)                     // blink LED as warning
+    for (i = 1; i < 7  ; i++)                // blink LED as warning
     {
-      digitalWrite(SDpower,HIGH);
+      digitalWrite(SDpower, HIGH);
       delay(150);
-      digitalWrite(SDpower,LOW);
+      digitalWrite(SDpower, LOW);
       delay(150);
     }
   }
-  digitalWrite(SDpower,LOW);
-  
-  //menu();                                        // goto menu
 
+  digitalWrite(SDpower, LOW);
+  digitalWrite(SD_CS,LOW);
+  
+  Serial.end();
+  
+  TWCR = bit(TWEN) | bit(TWIE) | bit(TWEA) | bit(TWINT);
+  //DDRD = DDRD | B11111100;
+   //DDRD = B11111111;
+   // set PORTD to low
+   PORTD = B00000000;
+   //PORTB = B00000000;
+   PORTC = B00000000;
+   
+    sleep.pwrDownMode(); //set sleep mode
+    sleep.sleepDelay(5); //sleep for: sleepTime
 }
 
 double get_distance() {
-  int i;
-  double dist;
-  double once;
+  double dist = 0;
+  int once = 0;
+  int count = 0;
   
-  pinMode(Pingpower,OUTPUT);                     // power for Ping
-  digitalWrite(Pingpower,HIGH);               // turn on power to sensor
-  
-  delay(200);
-  
-  for(i=1; i<=10; i++) {
-    once = ping();
-    dist += ping();
-  }
-  
-  digitalWrite(Pingpower, LOW);
+  echoSerial.begin(9600);
 
-  return (dist/10.0);
+  pinMode(Pingpower, OUTPUT);                    // power for Ping
+  digitalWrite(Pingpower, HIGH);              // turn on power to sensor
+
+  delay(350);
+  
+  for (i = 0; i < 10; i++) {
+    once = ping();
+    if (once > 1) {                            // guard against zero readings
+      dist += once;
+      count += 1;
+    }
+  }
+  lastDistance = ping();
+
+  pinMode(Pingpower, INPUT);                    // power for Ping
+  digitalWrite(Pingpower, LOW);
+  echoSerial.end();
+
+  if (count > 0) {
+  return (dist / count);
+  } else {
+    return -1;
+  }
 }
 
 double ping() {
   char dist[6];
+  char c;
   int state = 0;
   int j = 0;
-  dist[j] = 0;
-  
-  while (state < 3) {
-    if(echoSerial.available()) {
-      c = echoSerial.read();
+  unsigned long startTime = millis();
+  for(j=0; j< 6; j++) {
+    dist[j]=0;
+  }
 
+  j =  0;
+  while (state < 3 && (millis() - startTime) < 2000) {
+    if (echoSerial.available() ) {
+      c = echoSerial.read();
+      
       if (c == 'R') {
         state = 1;
-      } else if (c == 13) {
+      } else if (c == '\r' && state == 2) {
         state = 3;
-      } else if (state = 1) {
+      } 
+      else if (state == 1) {
         state = 2;
       }
       if (state == 2) {
         dist[j]  = c;
-        dist[j+1] = 0;
-        j++;
+        if (j < 6) {
+          j++;
+        }
       }
     }
   }
+ 
   return atof(dist);
 }
 
-//---------- read DS1337 RTC --------------------------------
-void readRTC()
-{
-  Wire.beginTransmission(DS1337ctrl);            // send i2c control identifier
-  Wire.write(0x00);                               // set register to 0
-  Wire.endTransmission();
-
-  Wire.requestFrom(DS1337ctrl, 7);               // read 7 bytes: secs,min,hr,dow,day,mth,yr
-  rtc_bcd[1] = Wire.read();
-  rtc_bcd[2] = Wire.read();
-  rtc_bcd[3] = Wire.read();
-  rtc_bcd[4] = Wire.read();
-  rtc_bcd[5] = Wire.read();
-  rtc_bcd[6] = Wire.read();
-  rtc_bcd[7] = Wire.read();
-  Wire.endTransmission();
-
-  for(int i=1; i<=7; i++)                        // convert BCD to decimal
-  {
-    rtc[i] = (rtc_bcd[i]/16)*10 + rtc_bcd[i]%16;
-  }
-
-  secs = rtc[1];
-  mins = rtc[2];
-  hrs = rtc[3];
-  days = rtc[5];
-  mnths = rtc[6];
-  yrs = rtc[7];
-  yrs = yrs + 2000;                              // change year to 20xx format
+void printDate() {
+    Serial.print(now.year());
+  Serial.print('-');
+  if (now.month() < 10) {Serial.print("0"); }
+    Serial.print(now.month());
+  Serial.print('-');
+  if (now.day() < 10) { Serial.print("0"); }
+    Serial.print(now.day());
+  Serial.print(' ');
+    Serial.print(now.hour());
+  Serial.print(':');
+  if (now.minute() < 10) { Serial.print("0"); }
+    Serial.print(now.minute());
+  Serial.print(':');
+  if (now.second() < 10) {Serial.print("0"); }
+    Serial.print(now.second());
+  Serial.print(' ');
 }
-//===================================================================
 
 //-------- print data to screen ---------------------------------
 void printData()
 {
+  Serial.begin(9600);
+  delay(100);
   Serial.print(IDnum);                           // Site identifier
   Serial.print(' ');
-  Serial.print(mnths);                           // date
-  Serial.print('-');
-  Serial.print(days);
-  Serial.print('-');
-  Serial.print(yrs);
-  Serial.print(' ');
-
-  Serial.print(hrs);                             // time
-  Serial.print(':');
-  if(mins < 10) Serial.print('0');
-  Serial.print(mins);
-  Serial.print(':');
-  if(secs < 10) Serial.print('0');
-  Serial.print(secs);
-  Serial.print(' ');
+  
+  printDate();
   
   Serial.print(distance);                      // distance, uncorrected
+  Serial.print(' ');
+
+  Serial.print(lastDistance);
+
+  Serial.print(' ');
+  Serial.print(name);
   Serial.println();
-  delay(10);
+  delay(100);
+  Serial.end();
 }
 //===============================================================
 
 // ------- store data to SDcard ---------------------------------------
 void storeData()
 {
-  
-  pinMode(SDpower, OUTPUT);                      // power for SDcard
-  pinMode(SD_CS,OUTPUT);
-  
-  digitalWrite(SDpower,HIGH);                    // turn on power to SDcard
   delay(500);
 
-  if (!SD.begin(SD_CS))                      // check SD card
-  {
-
-  }
-
+SD.begin(SD_CS);
+//  if (!SD.begin(SD_CS))  {                    // check SD card
+//      Serial.println("initialization failed!");
+//
+//  }
+  
   myfile = SD.open(name, FILE_WRITE);
-  delay(10);
 
   myfile.print(IDnum);                             // Site identifier
-  myfile.print(' ');
-  myfile.print(mnths);                             // date
+    myfile.print(',');
+
+  myfile.print(now.year());                           // date
   myfile.print('-');
-  myfile.print(days);
+  if (now.month() < 10) {myfile.print("0"); }
+  myfile.print(now.month());
   myfile.print('-');
-  myfile.print(yrs);
+  if (now.day() < 10) {myfile.print("0"); }
+  myfile.print(now.day());
   myfile.print(' ');
 
-  myfile.print(hrs);                               // hour
+  myfile.print(now.hour());                             // time
   myfile.print(':');
-  if(mins < 10) myfile.print('0');
-  myfile.print(mins);
+  if (now.minute() < 10) {myfile.print("0"); }
+  myfile.print(now.minute());
   myfile.print(':');
-  if(secs < 10) myfile.print('0');
-  myfile.print(secs);
-  myfile.print(' ');
+  if (now.second() < 10) {myfile.print("0"); }
+  myfile.print(now.second());
+
+  myfile.print(',');
 
   myfile.print(distance);                        // distance, uncorrected
-
+  myfile.print(',');
+  myfile.print(lastDistance);
   myfile.println();
 
   myfile.close();                                  // close file, write to SDcard
-  delay(200);                                      // give time to finish  
-
-  digitalWrite(SDpower,LOW);                       // turn off SDcard power
+  delay(500);                                      // give time to finish
+  
+  digitalWrite(SD_CS,LOW);
 }
 //==========================================================================
 
+void sleeping() {
+   // release TWI bus
+
+   TWCR = bit(TWEN) | bit(TWIE) | bit(TWEA) | bit(TWINT);
+  //DDRD = DDRD | B11111100;
+   //DDRD = B11111111;
+   // set PORTD to low
+   PORTD = B00000000;
+   //PORTB = B00000000;
+   PORTC = B00000000;
+   
+    sleep.pwrDownMode(); //set sleep mode
+    sleep.sleepDelay(sleepTime); //sleep for: sleepTime
+}
 
 void loop() {
-  // put your main code here, to run repeatedly:
- 
-  readRTC();                                     // read RTC
-
   distance = get_distance();
-  printData();                                 // output data to screen
-  storeData();                                 // store data to SD card
-    
-  //delay(1000);
   
-  //delay(sleepTime);
-  sleep.pwrDownMode(); //set sleep mode
-  sleep.sleepDelay(sleepTime); //sleep for: sleepTime
+  Wire.begin();
+  digitalWrite(SDpower, HIGH);                   // turn on power to SDcard
+  delay(10);
+  
+  now = rtc.now();
+  
+  //Serial.begin(9600);
+  storeData();                                 // store data to SD card
+  digitalWrite(SDpower, LOW);                      // turn off SDcard power
 
+  printData();                                 // output data to screen
+  
+  sleeping(); 
 }
-
-//--------------------------------------------------------
-void menu()
-{
-
-  addr = 1;                                      // read ID number from EEPROM
-  EEPROMbyte = EEPROM.read(addr);                // high byte
-  IDnum = EEPROMbyte * 256;
-  addr ++;                                       // next address
-  EEPROMbyte = EEPROM.read(addr);                // low byte
-  IDnum += EEPROMbyte;                           // combine
-
-    itoa(IDnum,a,10);                            // convert ID number to character array
-  name[4] = a[0];                                // put into file name[] array
-  name[5] = a[1];
-  name[6] = a[2];
-
-  Serial.println();                              // print out board info
-  Serial.print("Board number:  ");               // IDnum
-  Serial.println(IDnum);
-
-  Serial.print("Data filename: ");               // SD filename
-  Serial.println(name);
-
-  readRTC();
-  Serial.print("Current date:  ");
-  Serial.print(mnths);                           // date
-  Serial.print('-');
-  Serial.print(days);
-  Serial.print('-');
-  Serial.println(yrs);
-
-  Serial.print("Current time:  ");
-  Serial.print(hrs);                             // time
-  Serial.print(':');
-  if(mins < 10) Serial.print('0');
-  Serial.print(mins);
-  Serial.print(':');
-  if(secs < 10) Serial.print('0');
-  Serial.println(secs);
-
-  Serial.println();                              // menu options
-  Serial.println("1. Set clock");
-  Serial.println("2. Set ID number");
-  Serial.println();
-
-  timeout = millis() + 5000;                     // wait for input before timing out
-  while(millis() < timeout)
-  {
-    if(Serial.available())                       // if something typed, go to menu
-    {
-      menuinput = Serial.read();
-      break;
-    }
-  }
-
-  menuinput -= 48;                               // convert ASCII to decimal
-  switch(menuinput)
-  {
-  case 1:          //-------------------------------------------------------
-    Serial.println("Set clock:");                // set RTC
-
-    Serial.print("  input month:  ");
-    getinput();
-    mnths = indata;
-    Serial.print("  input day:    ");
-    getinput();
-    days = indata;
-    Serial.print("  input year:   ");
-    getinput();
-    yrs = indata;
-    Serial.print("  input hour:   ");
-    getinput();
-    hrs = indata;
-    Serial.print("  input minute: ");
-    getinput();
-    mins = indata;
-
-    mins = (mins/10)*16 + mins%10;               // convert decimal to BCD
-    hrs = (hrs/10)*16 + hrs%10;
-    days = (days/10)*16 + days%10;
-    mnths = (mnths/10)*16 + mnths%10;
-    yrs = (yrs/10)*16 + yrs%10;
-
-    Wire.beginTransmission(DS1337ctrl);          // set time, date
-    Wire.write(0);
-
-    Wire.write(secs);
-    Wire.write(mins);
-    Wire.write(hrs);
-    Wire.write(dow);
-    Wire.write(days);
-    Wire.write(mnths);
-    Wire.write(yrs);
-
-    Wire.endTransmission();
-
-    break;
-
-  case 2:          // ----------------------------------------------------
-    Serial.println("Set board ID number:");      // set board IB number
-
-    Serial.print("  input ID number: ");
-    getinput();
-    IDnum = indata;
-
-    addr = 1;                                    // store IDnum in EEPROM
-    EEPROMbyte = IDnum / 256;                    // high byte
-    EEPROM.write(addr,EEPROMbyte);
-    delay(20);
-    addr ++;
-    EEPROMbyte = IDnum % 256;                    // low byte
-    EEPROM.write(addr,EEPROMbyte);
-    delay(20);
-
-    break;
-  }
-
-  int k;
-  for(k=1; k<=5; k++)                            // print out 5 readings
-  {
-    readRTC();
-    /* readLM35(); */
-    ping();
-    printData();
-    delay(2000);
-  }
-  Serial.println();
-  Serial.println("ok ...");
-  Serial.println();
-  delay(5);
-}
-//================================================================
-
-
-//----------------- read user input -----------------------------------------------
-void getinput()
-{
-  int i;
-  timeout = millis() + 5000;                     // period to wait before timing out       
-
-  indata = 0;
-  while(millis() < timeout)
-  {
-    i = 0;
-    if(Serial.available())                       // if something availabe from serial port
-    {
-      input = Serial.read();                     // read input
-      if(input == 13) break;                     // if carraige return, done
-      if(input > 47 and input < 58)              // look for number between 0 and 9
-      {
-        input -= 48;                             // convert ASCII input to decimal value
-        Serial.print(input);
-        delay(5);
-        indata = indata*10 + input;              // concatenate multiple inputs
-      }
-      else
-      {
-        name[i] = input;
-        Serial.print(input);
-        i++;
-      }
-    }
-  }
-  Serial.println();
-  delay(5);
-}
-//===================================================================
 
